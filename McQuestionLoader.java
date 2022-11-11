053c1bbeba89a1977dc5f5a326a3940e073c607b
Niko <wrnkt1@gmail.com>
2022-11-11 15:34:57 -0500

    Handle load file not being found

--- McQuestionLoader.java
+++ McQuestionLoader.java
@@ -1,11 +1,12 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 
 class McQuestionLoader implements QuestionLoader {
 
     public McQuestionLoader() {
     }
      
-    public static McQuestion loadFromTextFile(File f) {
+    public static McQuestion loadFromTextFile(File f) throws FileNotFoundException {
         String[] answers = {"answer 1", "answer 2"};
         return new McQuestion("Test question", answers, 0);
 
@@ -20,8 +21,12 @@ class McQuestionLoader implements QuestionLoader {
     public static void main(String[] args) {
         log("Testing McQuestionLoader");
         File loadFile = new File("loadfile.txt");
-        McQuestion newQuestion = McQuestionLoader.loadFromTextFile(loadFile);
-        log(newQuestion.getPromptAndAnswers());
+        try {
+            McQuestion newQuestion = McQuestionLoader.loadFromTextFile(loadFile);
+            log(newQuestion.getPromptAndAnswers());
+        } catch (FileNotFoundException e) {
+            log("File not found.");
+        }
         
         log("-----------");
         
