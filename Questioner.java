a4a61cc98024057da3e610d18eebd86b46d66587
Niko <wrnkt1@gmail.com>
2022-11-11 23:06:06 -0500

    Constructor for Questioner

--- Questioner.java
+++ Questioner.java
@@ -1,4 +1,24 @@
+import java.util.ArrayList;
+
 class Questioner {
+
+    private ResponseTracker[] trackerList;
+
+    public Questioner(ArrayList<Question> questionList) {
+        ArrayList<ResponseTracker> trackerArrList = new ArrayList<ResponseTracker>();
+        for(Question q: questionList) {
+            trackerArrList.add(new ResponseTracker(q));
+        }
+        this.trackerList = trackerArrList.toArray(new ResponseTracker[trackerArrList.size()]);
+    }
+
+    /*
+    public Questioner(ArrayList<ResponseTracker> rt) {
+       // Questioner(ArrayList<ResponseTracker>) and Questioner(ArrayList<Question>) have the same erasure 
+
+       
+    }
+    */
 
     public static void main(String[] args) {
         log("test");
