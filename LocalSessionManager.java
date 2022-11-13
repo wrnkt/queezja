001dec0e314cbae8c87a8c7dff6921b657e8164b
Niko <wrnkt1@gmail.com>
2022-11-13 09:20:46 -0500

    Refactor LocalSessionManager

--- LocalSessionManager.java
+++ LocalSessionManager.java
@@ -12,34 +12,43 @@ class LocalSessionManager {
         q = new Questioner();
     };
 
-    {
-        try {
-            questionSet = mcql.loadFromJSON(getQuestionSet());
-            q.loadQuestions(questionSet);
-        } catch (FileNotFoundException e) {}
-    };
-    
-
-    // XXX: should a SessionManager manage multiple local sessions?
-    // probably not
     public LocalSessionManager() {
-        
     }
+
     // TODO: get a set of questions from a QuestionLoader
+    public void loadQuestionSet(String path) throws FileNotFoundException {
+        if (path.endsWith(".json")) {
+            questionSet = mcql.loadFromJSON(new File(path));
+        } else if (path.endsWith(".txt")) {
+            questionSet = mcql.loadFromTextFile(new File(path));
+        } else {
+            // ERROR
+
+        }
+        q.loadQuestions(questionSet);
+    }
+
+    // TODO: get user question set/file input
+    // path or file ?
+    public static String getQuestionSet() {
+        // call loadQuestionSet() here
+        return new String("");
+
+    }
 
     // TODO: load that set of questions into a Questioner
     // (that Questioner comes with an Analyzer)
 
     // TODO: prompt session beginning
     // TODO: allow choose a question set (file) (QuestionLoader func)
-    public static File getQuestionSet() {
-        return new File("loadfiles/testquestionset.json");
-
-    }
 
 
     public static void main(String[] args) {
         LocalSessionManager lsm = new LocalSessionManager();
+        try {
+        lsm.loadQuestionSet("loadfiles/testquestionset.json");
+        } catch (FileNotFoundException e) {
+        }
 
     }
 
