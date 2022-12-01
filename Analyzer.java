6eee5ed8b9c6dda5eb6c33acf82e891c952948b9
Niko <wrnkt1@gmail.com>
2022-12-01 14:08:30 -0500

    Add Questioner to Analyzer

--- Analyzer.java
+++ Analyzer.java
@@ -1,14 +1,19 @@
 class Analyzer
 {
+    
+    private final Questioner questioner;
 
     // TODO: Figure out \/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\
     // a Questioner should create and hold a personal Analyzer
     // should an Analyzer be allowed to be created with a Questioner
     // linked to it ? 
     // make a constructor that takes a Questioner ?
-    public Analyzer()
+    public Analyzer(Questioner questioner)
     {
+        this.questioner = questioner;
     }
+
+
 
     public static void main(String[] args)
     {
